d8a69b38059ed044256b040865fdb638a9ff5413
ziwanyu <yoojiwan@icloud.com>
2026-04-28 11:01:19 +0900

    feat: SignUp 화면 및 Feature 초기 구조 구현

---
+++ Projects/Feature/Onboarding/Sources/Signup/SignupFeature.swift
@@ -0,0 +1,8 @@
+import Foundation
+import ComposableArchitecture
+
+
+@Reducer
+struct SignupFeature: Reducer {
+    
+}
---
+++ Projects/Feature/Onboarding/Sources/Signup/SignupView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import ComposableArchitecture
+
+struct SignupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    SignupView()
+}
