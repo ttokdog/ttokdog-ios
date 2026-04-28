bc778c6d13643c8b157caf4af424085d3283c929
ziwanyu <yoojiwan@icloud.com>
2026-04-29 00:00:54 +0900

    feat: 회원가입 화면 State, Action, UI 초기 구현

--- Projects/Feature/Onboarding/Sources/Signup/SignupFeature.swift
+++ Projects/Feature/Onboarding/Sources/Signup/SignupFeature.swift
@@ -5,4 +5,124 @@ import ComposableArchitecture
 @Reducer
 struct SignupFeature: Reducer {
     
+    // MARK: - State
+    
+    @ObservableState
+    public struct State: Equatable {
+        // 입력값
+        public var id: String = ""
+        public var password: String = ""
+        public var passwordConfirm: String = ""
+        public var email: String = ""
+        public var nickname: String = ""
+        
+        // 아이디 중복확인
+        public var idCheckResult: IDCheckResult? = nil
+        public var isCheckingId: Bool = false
+        
+        // 비밀번호 표시
+        public var isPasswordVisible: Bool = false
+        public var isPasswordConfirmVisible: Bool = false
+        
+        
+        // 닉네임 중복확인
+        public var nicknameCheckResult: NicknameCheckResult? = nil
+        public var isCheckingNickname: Bool = false
+        
+        // 아이디 형식
+        public var isIdValid: Bool {
+            let regex = #"^[a-z0-9]{6,15}$"#
+            return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: id)
+        }
+        
+        // 비밀번호 형식
+        public var isPasswordValid: Bool {
+            let regex = #"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$"#
+            return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: password)
+        }
+        
+        // 이메일 형식
+        public var isEmailValid: Bool {
+            let regex = #"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+            return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: email)
+        }
+        
+        // 닉네임 형식
+        public var isNicknameValid: Bool {
+            let regex = #"^[가-힣a-zA-Z0-9]{1,10}$"#
+            return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: nickname)
+        }
+        
+        // 회원가입 활성화
+//        public var isSignUpEnabled: Bool {
+//            isIdValid
+//            && idCheckResult == .available
+//            && isPasswordValid
+//            && !isPasswordConfirmMismatch
+//            && !passwordConfirm.isEmpty
+//            && isEmailVerified
+//            && isNicknameValid
+//            && nicknameCheckResult == .available
+//        }
+        
+        
+        public init() {}
+    }
+
+    
+    // MARK: - Action
+    
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        
+        // 네비게이션
+        case backButtonTapped
+        
+        // 아이디
+        case checkDuplicateIdTapped
+        case checkDuplicateIdResponse(IDCheckResult)
+        case clearIdTapped
+        
+        
+        // 비밀번호
+        case togglePasswordVisibility
+        case togglePasswordConfirmVisibility
+        case clearPasswordTapped
+        case clearPasswordConfirmTapped
+        
+        // 이메일
+        case clearEmailTapped
+        
+        // 닉네임
+        case checkDuplicateNicknameTapped
+        case checkDuplicateNicknameResponse(NicknameCheckResult)
+        case clearNicknameTapped
+        
+        // 회원가입
+        case signUpTapped
+        
+    }
+
+    // MARK: - Reducer
+    
+    public var body: some ReducerOf<Self> {
+
+    }
+    
+
+    
+    
+    
+    public enum IDCheckResult: Equatable {
+        case available // 아이디 이용가능
+        case duplicate // 아이디 중복
+        case invalidFormat // 아이디 잘못된 형식
+    }
+    
+    public enum NicknameCheckResult: Equatable {
+        case available // 닉네임 이용가능
+        case duplicate // 닉네임 중복
+    }
+    
+
 }
--- Projects/Feature/Onboarding/Sources/Signup/SignupView.swift
+++ Projects/Feature/Onboarding/Sources/Signup/SignupView.swift
@@ -2,12 +2,82 @@ import SwiftUI
 import ComposableArchitecture
 
 struct SignupView: View {
+    
+    @Bindable public var store: StoreOf<SignupFeature>
+
+    public init(store: StoreOf<SignupFeature>) {
+        self.store = store
+    }
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ScrollView {
+            VStack(alignment: .leading, spacing: 30) {
+                // MARK: - 아이디
+                TtokdogTextField(text: $store.id,
+                                 title: "아이디",
+                                 description: "(6~15자 이내의 영문 소문자, 숫자 사용 가능)",
+                                 placeholder: "생성할 아이디를 입력해주세요")
+                
+                
+                // MARK: - 비밀번호
+                VStack(alignment: .leading, spacing: 6) {
+                    // TODO: 중복사용이 많아서 컴포넌트 분리하기
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text("비밀번호")
+                            .foregroundStyle(Color.gray700)
+                            .typography(.body9)
+                        Text("8~20자 이내의 영문/숫자/특수문자 혼합")
+                            .foregroundStyle(Color.gray500)
+                            .typography(.body11)
+                    }
+                    
+                    VStack(spacing: 6) {
+                        SignupPasswordInputField(text: $store.password,
+                                                 placeholder: "생성할 비밀번호를 입력해주세요",
+                                                 isVisible: store.isPasswordValid,
+                                                 isError: false,
+                                                 onToggleVisibility: { },
+                                                 onClear: { })
+                        
+                        SignupPasswordInputField(text: $store.passwordConfirm,
+                                                 placeholder: "비밀번호를 한번 더 입력해주세요",
+                                                 isVisible: store.isPasswordConfirmVisible,
+                                                 isError: false,
+                                                 onToggleVisibility: { },
+                                                 onClear: { })
+                    }
+                }
+                
+                // MARK: - 이메일
+                VStack {
+                    Text("이메일")
+                        .foregroundStyle(Color.gray700)
+                        .typography(.body9)
+                    HStack {
+                        
+                    }
+                    
+                }
+                
+                // MARK: - 닉네임
+                
+
+            }
+            .padding(20)
+            
+        } // 스크롤 블록
     }
 }
 
 // MARK: - Preview
 #Preview {
-    SignupView()
+    SignupView(
+        store: .init(
+            initialState: SignupFeature.State(),
+            reducer: { SignupFeature() }
+        )
+    )
 }
